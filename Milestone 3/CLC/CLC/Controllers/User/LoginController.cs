5599724f0824279bd407f3ec48de54675df8c839
JORDAN-PC\jordan <jordanr3>
2018-02-11 23:47:06 -0700

    M3

---
+++ Milestone 3/CLC/CLC/Controllers/User/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using CLC.Services.Business;
+using ICA3.Models;
+using ICA3.Models.Login;
+using ICA3.Services.Business;
+using ICA3.Services.Business.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICA3.Controllers.User
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View("Login");
+        }
+
+
+        [HttpPost]
+        public ActionResult doLogin(LoginRequest loginRequest)
+        {
+
+            LoginResponse response;
+
+            if (ModelState.IsValid) {
+            
+                SecurityService ss = new SecurityService();
+                response = ss.Authenticate(loginRequest);
+
+                if (response.Success)
+                {
+
+                    //load user model and throw in session var
+                    UserService userService = new UserService();
+                    var user = userService.loadUser(loginRequest);
+                    this.Session["user"] = user;
+
+
+                    return View("LoginPassed", loginRequest);
+
+                }
+            }
+            else
+            {
+                string errors = string.Join("<br/> ", ModelState.Values
+                                        .SelectMany(x => x.Errors)
+                                        .Select(x => x.ErrorMessage));
+                response = new LoginResponse(false, errors);
+            }
+            return View("LoginFailed", response);
+
+
+        }
+
+    }
+}
---
+++ Milestone 3/CLC/CLC/Services/Data/Login/SecurityDAO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using ICA3.Models;
+using System.Data.SqlClient;
+using System.Data;
+using ICA3.Models.Login;
+using ICA3.Models.Register;
+using ICA3.Constants;
+
+namespace Activity3.Services.Data.Login
+{
+    public class SecurityDAO
+    {
+
+        
+        public Boolean validUser(LoginRequest loginRequest)
+        {
+             bool result = false;
+
+             try
+             {
+                 // Setup SELECT query with parameters
+                 string query = "SELECT * FROM dbo.users WHERE USERNAME=@Username AND PASSWORD=@Password";
+
+                 // Create connection and command
+                 using (SqlConnection cn = new SqlConnection(DB.CONNECTION_STRING))
+                 using (SqlCommand cmd = new SqlCommand(query, cn))
+                 {
+                     // Set query parameters and their values
+                     cmd.Parameters.Add("@Username", SqlDbType.VarChar, 50).Value = loginRequest.Username;
+                     cmd.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = loginRequest.Password;
+
+                     // Open the connection
+                     cn.Open();
+
+                     // Using a DataReader see if query returns any rows
+                     SqlDataReader reader = cmd.ExecuteReader();
+                     if (reader.HasRows)
+                         result = true;
+                     else
+                         result = false;
+
+                     // Close the connection
+                     cn.Close();
+                 }
+
+             }
+             catch (SqlException e)
+             {
+                 // TODO: should log exception and then throw a custom exception
+                 throw e;
+             }
+
+             // Return result of finder
+             return result;
+        }
+
+       
+    }
+}
